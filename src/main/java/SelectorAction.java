918a64d85665150a435b0b0332e325ff1f801d0b
mohak1712 <mohakpuri1712@gmail.com>
2020-06-07 20:14:11 +0530

    show tool window on event trigger

--- src/main/java/SelectorAction.java
+++ src/main/java/SelectorAction.java
@@ -2,6 +2,8 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import org.jetbrains.annotations.NotNull;
 
 public class SelectorAction extends AnAction {
@@ -9,15 +11,20 @@ public class SelectorAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         Editor edit = e.getData(PlatformDataKeys.EDITOR);
         String selectedText = edit.getSelectionModel().getSelectedText();
+        ToolWindow toolWindow = ToolWindowManager.getInstance(e.getProject()).getToolWindow("CodeSnapshot");
+
+        if (!toolWindow.isVisible())
+            toolWindow.show(() -> {
+            });
     }
 
     @Override
     public void update(@NotNull AnActionEvent e) {
         Editor edit = e.getData(PlatformDataKeys.EDITOR);
-        e.getPresentation().setEnabledAndVisible(isEnabled(edit));
+        e.getPresentation().setEnabledAndVisible(shouldEnable(edit));
     }
 
-    private boolean isEnabled(Editor edit) {
+    private boolean shouldEnable(Editor edit) {
         return edit != null && edit.getSelectionModel().getSelectedText() != null
                 && edit.getSelectionModel().getSelectedText().length() > 0;
     }
