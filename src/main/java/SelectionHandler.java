d584e83a2e2777e09bc59ab69067ffcbd97e56ea
mohak1712 <mohakpuri1712@gmail.com>
2020-06-14 13:13:47 +0530

    merges selection event and selection handler

--- src/main/java/SelectionHandler.java
+++ src/main/java/SelectionHandler.java
@@ -1,3 +1,6 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.event.EditorMouseEvent;
 import com.intellij.openapi.editor.event.EditorMouseMotionListener;
@@ -9,24 +12,50 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.awt.*;
 
-public class SelectionHandler implements EditorMouseMotionListener {
+public class SelectionHandler extends AnAction implements EditorMouseMotionListener {
 
     @Override
     public void mouseDragged(@NotNull EditorMouseEvent e) {
         Editor editor = e.getEditor();
         String selectedText = editor.getSelectionModel().getSelectedText();
-
         if (selectedText == null || selectedText.isEmpty()) {
             return;
         }
 
-        ToolWindow toolWindow = ToolWindowManager.getInstance(editor.getProject())
-                .getToolWindow("CodeSnapshot");
-
+        ToolWindow toolWindow = ToolWindowManager.getInstance(editor.getProject()).getToolWindow("CodeSnapshot");
         if (!toolWindow.isVisible()) {
             return;
         }
 
+        update(editor, selectedText, toolWindow);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Editor edit = e.getData(PlatformDataKeys.EDITOR);
+        String selectedText = edit.getSelectionModel().getSelectedText();
+        ToolWindow toolWindow = ToolWindowManager.getInstance(e.getProject()).getToolWindow("CodeSnapshot");
+
+        if (!toolWindow.isVisible()) {
+            toolWindow.show(() -> {
+            });
+        }
+
+        update(edit, selectedText, toolWindow);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Editor edit = e.getData(PlatformDataKeys.EDITOR);
+        e.getPresentation().setEnabledAndVisible(shouldEnable(edit));
+    }
+
+    private boolean shouldEnable(Editor edit) {
+        return edit != null && edit.getSelectionModel().getSelectedText() != null
+                && edit.getSelectionModel().getSelectedText().length() > 0;
+    }
+
+    private void update(Editor editor, String selectedText, ToolWindow toolWindow) {
         Content content = toolWindow.getContentManager().getContent(0);
         JPanel component = (JPanel) content.getComponent();
         JScrollPane scrollPane = (JScrollPane) component.getComponent(0);
