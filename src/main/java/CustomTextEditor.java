445af3cfff878eb99b22b90631cccb52ca191b8b
mohak1712 <mohakpuri1712@gmail.com>
2020-07-04 13:33:27 +0530

    consumes all mouse events preventing invocation of SelectionHandler

--- src/main/java/CustomTextEditor.java
+++ src/main/java/CustomTextEditor.java
@@ -1,7 +1,11 @@
+import com.intellij.openapi.editor.event.EditorMouseEvent;
+import com.intellij.openapi.editor.event.EditorMouseListener;
+import com.intellij.openapi.editor.event.EditorMouseMotionListener;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.EditorTextField;
+import org.jetbrains.annotations.NotNull;
 
 public class CustomTextEditor extends EditorTextField {
 
@@ -16,6 +20,45 @@ public class CustomTextEditor extends EditorTextField {
         editor.setInsertMode(false);
         editor.setCaretEnabled(false);
         editor.setViewer(true);
+        editor.addEditorMouseMotionListener(new EditorMouseMotionListener() {
+            @Override
+            public void mouseMoved(@NotNull EditorMouseEvent e) {
+                e.consume();
+            }
+
+            @Override
+            public void mouseDragged(@NotNull EditorMouseEvent e) {
+                e.consume();
+            }
+        });
+
+        editor.addEditorMouseListener(new EditorMouseListener() {
+            @Override
+            public void mousePressed(@NotNull EditorMouseEvent event) {
+                event.consume();
+            }
+
+            @Override
+            public void mouseClicked(@NotNull EditorMouseEvent event) {
+                event.consume();
+            }
+
+            @Override
+            public void mouseReleased(@NotNull EditorMouseEvent event) {
+                event.consume();
+            }
+
+            @Override
+            public void mouseEntered(@NotNull EditorMouseEvent event) {
+                event.consume();
+            }
+
+            @Override
+            public void mouseExited(@NotNull EditorMouseEvent event) {
+                event.consume();
+            }
+        });
+
         return editor;
     }
 }
