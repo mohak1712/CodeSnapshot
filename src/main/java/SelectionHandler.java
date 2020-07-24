0ec49f49119bba4d46b522c456b2eb58610a4bda
mohak1712 <mohakpuri1712@gmail.com>
2020-07-24 21:06:05 +0530

    replaces mouse listener with editor level selection listener

--- src/main/java/SelectionHandler.java
+++ src/main/java/SelectionHandler.java
@@ -3,8 +3,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.event.EditorMouseEvent;
-import com.intellij.openapi.editor.event.EditorMouseMotionListener;
+import com.intellij.openapi.editor.event.SelectionEvent;
+import com.intellij.openapi.editor.event.SelectionListener;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.psi.PsiDocumentManager;
@@ -15,10 +15,32 @@ import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import org.jetbrains.annotations.NotNull;
 
-public class SelectionHandler extends AnAction implements EditorMouseMotionListener {
+public class SelectionHandler extends AnAction {
 
     @Override
-    public void mouseDragged(@NotNull EditorMouseEvent e) {
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        String selectedText = editor.getSelectionModel().getSelectedText();
+        editor.getSelectionModel().addSelectionListener(new SelectionListener() {
+            @Override
+            public void selectionChanged(@NotNull SelectionEvent e) {
+                updateViewOnSelectionChanged(e);
+            }
+        });
+
+        ToolWindow toolWindow = ToolWindowManager.getInstance(e.getProject()).getToolWindow("CodeSnapshot");
+        if (!toolWindow.isVisible()) {
+            toolWindow.show(() -> {
+            });
+        }
+
+        JBPanel panel = getPanel(toolWindow);
+        updateUi(panel);
+        updateView(psiFile, selectedText, (EditorTextField) panel.getComponent(1));
+    }
+
+    private void updateViewOnSelectionChanged(@NotNull SelectionEvent e) {
         Editor editor = e.getEditor();
         PsiFile psiFile = PsiDocumentManager.getInstance(editor.getProject()).getPsiFile(editor.getDocument());
         String selectedText = editor.getSelectionModel().getSelectedText();
@@ -37,6 +59,11 @@ public class SelectionHandler extends AnAction implements EditorMouseMotionListe
         updateView(psiFile, selectedText, (EditorTextField) panel.getComponent(1));
     }
 
+    private void updateUi(JBPanel panel) {
+        panel.getComponent(0).setVisible(false);
+        panel.getComponent(1).setVisible(true);
+    }
+
     private void updateView(PsiFile psiFile, String selectedText, EditorTextField codeView) {
         if (selectedText.startsWith("\n") && selectedText.endsWith("\n"))
             codeView.setText(selectedText);
@@ -48,28 +75,6 @@ public class SelectionHandler extends AnAction implements EditorMouseMotionListe
             codeView.setText("\n" + selectedText + "\n");
 
         codeView.setFileType(psiFile.getFileType());
-    }
-
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Editor editor = e.getData(PlatformDataKeys.EDITOR);
-        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-        String selectedText = editor.getSelectionModel().getSelectedText();
-        ToolWindow toolWindow = ToolWindowManager.getInstance(e.getProject()).getToolWindow("CodeSnapshot");
-
-        if (!toolWindow.isVisible()) {
-            toolWindow.show(() -> {
-            });
-        }
-
-        JBPanel panel = getPanel(toolWindow);
-        updateUi(panel);
-        updateView(psiFile, selectedText, (EditorTextField) panel.getComponent(1));
-    }
-
-    private void updateUi(JBPanel panel) {
-        panel.getComponent(0).setVisible(false);
-        panel.getComponent(1).setVisible(true);
     }
 
     @Override
